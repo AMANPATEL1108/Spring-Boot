10c39e6e5f4808f21003fcf58c0fd11853915bde
AMANPATEL <amanpatel8844382@gmail.com>
2024-07-07 11:42:43 +0530

    simple files  to

--- 01/springbootproject/src/main/java/com/first/springbootproject/SpringbootprojectApplication.java
+++ 01/springbootproject/src/main/java/com/first/springbootproject/SpringbootprojectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootprojectApplication {
 
 	public static void main(String[] args) {
-
+//
 		SpringApplication.run(SpringbootprojectApplication.class, args);
 		System.out.println("First starter..........");
 	}
